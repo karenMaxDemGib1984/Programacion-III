346d48febbf71aad4314463c58cfda424b28b965
karenMaxDemGib1984 <o4karenportillo@gmail.com>
2025-07-29 19:19:14 -0600

    PrimerCommit

---
+++ Modulos/main.py
@@ -0,0 +1,13 @@
+import Package.modul as Modulo
+
+
+v1 = float(input("Ingresa un valor "))
+v2 = float(input("ingresa otro valor "))
+items = Modulo.opbasicas(v1, v2)
+
+print("La suma es igual a " + str(items["suma"]))
+print("La resta es igual a " +str(items["resta"]))
+print("La multiplicación  es igual a " +str(items["mul"]))
+print("La división es igual a " +str(items["div"]))
+print("La elevación del primer numero al segundo es de " +str(items["cua"]))
+print("El modulo de dividir es igual a " +str(items["div"]))
