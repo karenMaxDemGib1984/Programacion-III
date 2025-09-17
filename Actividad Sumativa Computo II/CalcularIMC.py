04bac3da0bf70d8a834712b41355d4da33c7b9ae
karenMaxDemGib1984 <o4karenportillo@gmail.com>
2025-09-16 20:17:12 -0600

    ComboBox

--- Actividad Sumativa Computo II/CalcularIMC.py
+++ Actividad Sumativa Computo II/CalcularIMC.py
@@ -1,8 +1,14 @@
 #import PyQt5
-from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QVBoxLayout, QPushButton,QLineEdit, QMessageBox)
+from PyQt5.QtWidgets import (QApplication, QLabel, QWidget, QVBoxLayout, QPushButton,QLineEdit, QMessageBox, QComboBox)
 
 import sys 
 
+def calcularIMC(peso, estatura):
+    pass
+    
+    
+    
+  
 
 app = QApplication(sys.argv)
 ventana = QWidget()
@@ -11,16 +17,21 @@ ventana = QWidget()
 ventana.setWindowTitle("Calcular IMC")
 ventana.setGeometry(100,100,300,200)
 layout = QVBoxLayout()
-peso = QLabel("Ingresa tu peso (kg)  ")
-estatura = QLabel("Ingresa tu estatura(m)")
+peso = QLabel("Ingresa tu peso ")
+opcionPeso = QComboBox()
+opcionPeso.addItems(["kg","lb"])
+estatura = QLabel("Ingresa tu estatura")
+opcionEstatura = QComboBox()
+opcionEstatura.addItems(["m","cm"])
+
 Peso = QLineEdit()
 Estat = QLineEdit()
 boton = QPushButton("Calcular")
 #boton.clicked.connect(mensaje)
 
-widgets = [peso, Peso, estatura, Estat , boton]
+widgets = [peso, Peso, opcionPeso, estatura, opcionEstatura, Estat , boton]
+ventana.setLayout(layout)
 for w in widgets:
     layout.addWidget(w)
 ventana.show()
 sys.exit(app.exec_())
- 
